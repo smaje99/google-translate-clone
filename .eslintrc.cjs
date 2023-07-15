e2705ad8c2be0c94e724f89b6404e6b53e3dfb7c
Sergio Majé <smajefranco@gmail.com>
2023-07-15 18:26:59 -0500

    🔧 Add prettier to project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,35 +1,37 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'standard-with-typescript',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
     },
-    "extends": [
-        "standard-with-typescript",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module",
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "react/react-in-jsx-scope": "off",
-        "react/prop-types": "off"
-    }
-}
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'max-len': ['error', 90],
+    quotes: [2, 'single', { avoidEscape: true }],
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    curly: 'error',
+  },
+};
